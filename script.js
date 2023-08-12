370a5a032a7f6db071689c8c885043742376629b
codescalper <msfunbook@gmail.com>
2023-08-12 12:42:02 +0530

    Added node_modules

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
-    import {v4} from './node_modules/uuid/dist/esm-browser/index.js';
+        import {v4} from 'uuid';
 
 
-    export let generatePassword = ()=>{
-        const easyRadio = document.querySelector("#easy");
-        const mediumRadio = document.querySelector("#medium");
-        const hardRadio = document.querySelector("#hard");
+        export let generatePassword = ()=>{
+            const easyRadio = document.querySelector("#easy");
+            const mediumRadio = document.querySelector("#medium");
+            const hardRadio = document.querySelector("#hard");
 
-        if(easyRadio.checked){
-            console.log("easy")
-        }else if (mediumRadio.checked){
-            console.log("medium")
-        }else if(hardRadio.checked){
-            console.log("Hard")
-        }else{
-            console.log("select something")
+            if(easyRadio.checked){
+                console.log("easy")
+            }else if (mediumRadio.checked){
+                console.log("medium")
+            }else if(hardRadio.checked){
+                console.log("Hard")
+            }else{
+                console.log("select something")
+            }
+
+            var selectedVal = 7;
+
+            if(easyRadio.checked){
+                selectedVal = parseInt(easyRadio.value);
+            }else if(mediumRadio.checked){
+                selectedVal = parseInt(mediumRadio.value);
+            }else{
+                selectedVal = parseInt(hardRadio.value);
+            }
+
+            const password = v4().substr(0, selectedVal);
+            console.log(`${password}`)
+            let pass = document.getElementById('password')
+            pass.value = password
+
         }
 
-        var selectedVal = 7;
-
-        if(easyRadio.checked){
-            selectedVal = parseInt(easyRadio.value);
-        }else if(mediumRadio.checked){
-            selectedVal = parseInt(mediumRadio.value);
-        }else{
-            selectedVal = parseInt(hardRadio.value);
-        }
-
-        const password = v4().substr(0, selectedVal);
-        console.log(`${password}`)
-        let pass = document.getElementById('password')
-        pass.value = password
-
-    }
-
-    let button = document.querySelector(".genButton");
-    button.addEventListener("click", generatePassword);
+        let button = document.querySelector(".genButton");
+        button.addEventListener("click", generatePassword);
 
 
